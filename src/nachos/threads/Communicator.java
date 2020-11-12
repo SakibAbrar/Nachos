78dc2ca6ad53495b61b5e7afd0ce8c1a4cf171e7
SakibArar <sakib.cghs@gmail.com>
2020-11-12 23:15:02 +0600

    Task 4 is done!

--- src/nachos/threads/Communicator.java
+++ src/nachos/threads/Communicator.java
@@ -6,7 +6,7 @@ import nachos.machine.*;
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
  * messages. Multiple threads can be waiting to <i>speak</i>,
  * and multiple threads can be waiting to <i>listen</i>. But there should never
- * be a time when both a speaker and a listener are waiting, because the two
+ * be a time when both a speakerCondition and a listenerCondition are waiting, because the two
  * threads can be paired off at this point.
  */
 public class Communicator {
@@ -18,15 +18,31 @@ public class Communicator {
 
     /**
      * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
+     * <i>word</i> to the listenerCondition.
      *
      * <p>
      * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
+     * Exactly one listenerCondition should receive <i>word</i>.
      *
-     * @param	word	the integer to transfer.
+     * @param	msg	the integer to transfer.
      */
-    public void speak(int word) {
+    public void speak(int msg) {
+        // Aquire the lock
+        sharedLock.acquire();
+
+        // forcing the speakerCondition to wait until an audience comes.
+        while (msgBuffer != null) {
+            speakerCondition.sleep();
+        }
+
+        msgBuffer = msg;
+
+        listenerCondition.wake();
+
+        returnCondition.sleep();
+
+        // releasing the lock
+        sharedLock.release();
     }
 
     /**
@@ -36,6 +52,34 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
-	return 0;
+        int wordToReturn;
+
+        // Aquire the lock
+        sharedLock.acquire();
+
+        //
+        while (msgBuffer == null)
+        {
+            listenerCondition.sleep();
+        }
+
+        wordToReturn = msgBuffer.intValue();
+        msgBuffer = null;
+
+        speakerCondition.wake();
+
+        returnCondition.wake();
+
+        // release the lock
+        sharedLock.release();
+
+        return wordToReturn;
     }
+
+    private Lock sharedLock = new Lock();
+    private Condition2 speakerCondition = new Condition2(sharedLock);
+    private Condition2 listenerCondition = new Condition2(sharedLock);
+    private Condition2 returnCondition = new Condition2(sharedLock);
+    private Integer msgBuffer = null;
+
 }
